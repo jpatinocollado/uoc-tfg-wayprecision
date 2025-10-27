56846ffb8f9ddae2a9dcf09592595ab3eb7fbf5c
Patiño Collado, Jesús <jpatinoc@minsait.com>
2025-10-27 20:26:21 +0100

    Images, Colors and Menu

--- WayPrecision/AppShell.xaml.cs
+++ WayPrecision/AppShell.xaml.cs
@@ -2,9 +2,24 @@
 {
     public partial class AppShell : Shell
     {
+        public Command OpenEmailCommand { get; }
+
         public AppShell()
         {
             InitializeComponent();
+
+            OpenEmailCommand = new Command(async () =>
+            {
+                await Launcher.OpenAsync("mailto:jpcollado@uoc.edu");
+            });
+
+            BindingContext = this;
+        }
+
+        private static async void OnLinkedInTapped(object sender, EventArgs e)
+        {
+            var url = "https://www.linkedin.com/in/jesuspatinocollado/";
+            await Launcher.Default.OpenAsync(url);
         }
     }
 }
---
+++ WayPrecision/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace WayPrecision;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
