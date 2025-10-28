d2267e678c283e4f4b95a2a5696b4860e06615f5
Patiño Collado, Jesús <jpatinoc@minsait.com>
2025-10-28 19:31:41 +0100

    Obtener la posición del sensor del GPS

---
+++ WayPrecision/Domain/Components/WebViewMessageManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayPrecision.Domain.Components
+{
+    public static class WebViewMessageManager
+    {
+        public static void EvaluateMessage(string message)
+        {
+            string[] messages = message.Split(';');
+            string evento = messages[0];
+            string datos = messages.Length > 1 ? messages[1] : string.Empty;
+            if (Application.Current != null &&
+                Application.Current.MainPage is Shell shell &&
+                            shell.CurrentPage is MainPage mainPage)
+            {
+                switch (evento)
+                {
+                    case "enableLocation":
+                        mainPage.SetEnableLocation(true);
+                        break;
+
+                    case "disableLocation":
+                        mainPage.SetEnableLocation(false);
+                        break;
+
+                    case "enableCenterLocation":
+                        mainPage.SetEnableCenterLocation(true);
+                        break;
+
+                    case "disableCenterLocation":
+                        mainPage.SetEnableCenterLocation(false);
+                        break;
+
+                    case "setZoom":
+                        mainPage.SetZoom(datos);
+                        break;
+
+                    default:
+                        Application.Current.MainPage.DisplayAlert("JS -> C#", message, "Aceptar");
+                        break;
+                }
+            }
+        }
+    }
+}
