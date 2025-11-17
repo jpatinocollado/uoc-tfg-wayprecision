572d3af7f3cf8887608e1425976b3fae68865960
Patiño Collado, Jesús <jpatinoc@minsait.com>
2025-11-17 08:56:03 +0100

    jquery loading

---
+++ WayPrecision/Resources/Raw/Javascript/LoadingManagerService.js
@@ -0,0 +1,20 @@
+﻿var LoadingManagerService = (function () {
+    var container = '#map';
+    var show = function (message) {
+        $(container).loading({
+            message: message,
+            showClass: 'loadingShow'
+        });
+    }
+
+    var hide = function () {
+        if ($(container).is(':loading'))
+            $(container).loading('stop');
+    }
+
+    //return plublic methods of service
+    return {
+        Show: show,
+        Hide: hide
+    };
+})();
